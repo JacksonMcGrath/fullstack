810144082595c9021ad86ffa854fd5efe2d33931
JacksonMcgrath <jacksonfmcgrath@gmail.com>
2018-01-12 09:46:50 -0600

    added ejs and formatted new.ejs page and it renders correctly

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 
 
+app.get('/fruits/new', (req , res) => {
+	res.render('new.ejs');
+})
+
 
 app.listen(3000, () => {
 	console.log('server listening on 3000');
