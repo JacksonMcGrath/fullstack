1da3446b10b8c8508ca8faeae26d055fd18b370f
JacksonMcgrath <jacksonfmcgrath@gmail.com>
2018-01-12 10:02:35 -0600

    req.body is setup

--- server.js
+++ server.js
@@ -2,8 +2,27 @@ const express = require('express');
 const app = express();
 
 
+// middleware
+
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// new route
 app.get('/fruits/new', (req , res) => {
 	res.render('new.ejs');
+})
+
+// create route
+app.post('/fruits', (req , res) => {
+	// form results live in req.body
+	if(req.body.readyToEat === 'on') {
+		req.body.readyToEat = true;
+	} else {
+		req.body.readyToEat = false;
+	}
+
+	res.send(req.body)
 })
 
 
