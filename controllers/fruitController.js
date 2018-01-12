60ed28d150b1013fcc2740bc7fe239b6627acb63
JacksonMcgrath <jacksonfmcgrath@gmail.com>
2018-01-12 16:15:15 -0600

    added edit page that is conected and working with the database

--- controllers/fruitController.js
+++ controllers/fruitController.js
@@ -15,12 +15,14 @@ router.get('/', (req, res) => {
 	})
 })
 
-// new route
+// ROUTES
+
+// NEW 
 router.get('/new', (req , res) => {
 	res.render('new.ejs')
 })
 
-// create route
+// CREATE
 router.post('/', (req, res) => {
 	// form results live in req.body
 	if(req.body.readyToEat === 'on') {
@@ -38,7 +40,7 @@ router.post('/', (req, res) => {
 	// res.send(req.body)
 })
 
-// show route
+// SHOW
 router.get('/:id', (req, res) => {
 	// get the fruit from db
 	Fruit.findById(req.params.id, (err, fruitFound) => {
@@ -48,5 +50,42 @@ router.get('/:id', (req, res) => {
 		})
 	})
 })
+
+// DELETE
+router.delete('/:id', (req , res) => {
+
+	Fruit.findByIdAndRemove(req.params.id, (err, data) => {
+		if(err) {
+			console.log(err);
+		} else {
+			res.redirect('/fruits')
+		}
+	})	
+
+	console.log(req.body, 'req.body', req.params);
+})
+
+// EDIT
+router.get('/:id/edit', (req , res) => {
+	Fruit.findById(req.params.id, (err, data) => {
+		res.render('edit.ejs', {
+			fruit: data
+		})	
+	})
+})
+
+router.put('/:id', (req , res) => {
+
+	if(req.body.readyToEat === 'on') {
+		req.body.readyToEat = true;
+	} else {
+		req.body.readyToEat = false;
+	}
+
+	Fruit.findByIdAndUpdate(req.params.id, req.body, (err, updateData) => {
+		res.redirect('/fruits/')
+	})
+})
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const methodOverride = require('method-Override')
 
 // DB
 mongoose.connect('mongodb://localhost:27017/fruits', { useMongoClient: true })
@@ -10,13 +11,15 @@ mongoose.connection.once('open', () => {
 	console.log('connected to mongo');
 })
 
-// middleware
+// MIDDLEWARE
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'))
 
-// controller
+// CONTROLLER
 const fruitController = require('./controllers/fruitController');
 app.use('/fruits/', fruitController);
 
+// Localhost listener
 app.listen(3000, () => {
 	console.log('server listening on 3000');
 })
